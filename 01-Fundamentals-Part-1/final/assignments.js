846c182b5fbeb84601afc1ba60a31a4847af1141
Swetha-Raghunath- <swetha.blore@gmail.com>
2023-09-15 12:58:16 -0400

    First Commit - Types of Variables , operators

---
+++ 01-Fundamentals-Part-1/final/assignments.js
@@ -0,0 +1,24 @@
+//assignment - Chapter 1 //
+
+const country = "Bharath";
+const continent = "Asia";
+let population = 1000000000;
+
+let isIsland = false;
+let language;
+
+console.log("My country is : " + country + " with population of " + population + " and being in the continent " + continent + " is it an island:? " + isIsland + " with its language being " + language);
+
+language = "Kannada";
+
+console.log("My country is : " + country + " with population of " + population + " and being in the continent " + continent + " is it an island:? " + isIsland + " with its language being " + language);
+
+
+// assignment - Basic Operators
+console.log("half of the population of " + country + "is:" + population / 2);
+console.log(population++);
+const finlandPopulation = 600000000;
+console.log(population > finlandPopulation);
+const averagePopulation = 3300000000;
+console.log(population < averagePopulation);
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
---
+++ 01-Fundamentals-Part-1/final/challenges.js
@@ -0,0 +1,8 @@
+/////coding challenges - 1////
+
+const markHeight = 1.69;
+const marWeight = 78;
+const JohnHeight = 1.95;
+const JohnWeight = 92;
+
+const markBMI = marWeight / markHeight ** 2;
